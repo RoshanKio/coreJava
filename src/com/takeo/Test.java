fd983cdb402423f97397d42a00e521df23c92c0f
bhipinder <bhupinder@gmail@gmail.com>
2023-12-18 21:41:07 -0500

    my COmmit

---
+++ src/com/takeo/Test.java
@@ -0,0 +1,17 @@
+package com.takeo;
+
+public class Test {
+	public static void main(String ry[]) {
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.exit(0);
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+		System.out.println("Hello, How are you ?");
+	}
+}
